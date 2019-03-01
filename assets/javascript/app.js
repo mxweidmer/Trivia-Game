3502b6a669e007870ba622b012e4cef23e3de3a1
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-01 15:02:40 -0800

    added initial variables (array of question objects, num of correct questions, num of incorrect questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,64 @@
 
 
+
+$(document).ready(function () {
+
+    var correct = 0;
+    var incorrect = 0;
+    var questions =
+        [
+            {
+                question: "a question",
+                answers: {
+                    answer: "1",
+                    answer: "2",
+                    trueAnswer: "3",
+                    answer: "4"
+                }
+            },
+            {
+                question: "a second question",
+                answers: {
+                    answer: "5",
+                    answer: "6",
+                    trueAnswer: "7",
+                    answer: "8"
+                }
+            },
+            {
+                question: "a second question",
+                answers: {
+                    answer: "5",
+                    answer: "6",
+                    trueAnswer: "7",
+                    answer: "8"
+                }
+            },
+            {
+                question: "a second question",
+                answers: {
+                    answer: "5",
+                    answer: "6",
+                    trueAnswer: "7",
+                    answer: "8"
+                }
+            },
+            {
+                question: "a second question",
+                answers: {
+                    answer: "5",
+                    answer: "6",
+                    trueAnswer: "7",
+                    answer: "8"
+                }
+            }
+        ]
+
+
+    console.log(questions[1].answers.answer);
+    console.log(questions[1].answers.trueAnswer);
+    console.log(questions[1].question);
+
+
+});
+
