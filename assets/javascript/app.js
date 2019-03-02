1342fc8e57954292c3b728858229c13052b99408
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-01 20:39:06 -0800

    got button listener working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,55 +5,59 @@ $(document).ready(function () {
 
     var correct = 0;
     var incorrect = 0;
+    var questionCount = 0;
+    var questionIndex = 0;
 
     var questions =
         [
             {
-                question: "What is the capitol of the US?",
+                question: "What is the capital city of the United States?",
                 answers: [
                     [false, "New York City"],
                     [true, "Washington DC"],
                     [false, "Chicago"],
-                    [false, "San Francisco"]
+                    [false, "Los Angeles"]
                 ]
             },
             {
-                question: "a question",
+                question: "What is the player character of Fallout: New Vegas known as in the Movaje Wasteland?",
                 answers: [
-                    [false, "answer"],
-                    [true, "answer"],
-                    [false, "answer"],
-                    [false, "answer"]
+                    [false, "The Lone Wanderer"],
+                    [false, "The Sole Survivor"],
+                    [false, "The Vault Dweller"],
+                    [true, "The Courier"]
                 ]
             },
             {
-                question: "a question",
+                question: "What is the largest planet in the solar system?",
                 answers: [
-                    [false, "answer"],
-                    [true, "answer"],
-                    [false, "answer"],
-                    [false, "answer"]
+                    [false, "Pluto"],
+                    [false, "Earth"],
+                    [false, "Saturn"],
+                    [true, "Jupiter"]
                 ]
             },
             {
-                question: "a question",
+                question: "Who is the main antagonist of the Harry Potter Series?",
                 answers: [
-                    [false, "answer"],
-                    [true, "answer"],
-                    [false, "answer"],
-                    [false, "answer"]
+                    [true, "Voldemort"],
+                    [false, "Darth Vader"],
+                    [false, "Terminator"],
+                    [false, "The Joker"]
                 ]
             },
             {
-                question: "a question",
+                question: "Who wrote The Shining?",
                 answers: [
-                    [false, "answer"],
-                    [true, "answer"],
-                    [false, "answer"],
-                    [false, "answer"]
+                    [false, "J.K. Rowling"],
+                    [false, "R.L. Stein"],
+                    [true, "Stephen King"],
+                    [false, "J.R.R. Tolkein"]
                 ]
             }
         ]
+
+    var posArray = ["#top-left", "#top-right", "#bottom-left", "#bottom-right"]
 
 
     function createButton(answerType) {
@@ -64,36 +68,39 @@ $(document).ready(function () {
         return button;
     }
 
-    function createQuestionInterface(questionIndex) {
+    function createQuestionInterface() {
 
-        var buttonDiv = $("<div>");
+        $("#communication").text(questions[questionIndex].question);
 
         for (var i = 0; i < questions[questionIndex].answers.length; i++) {
             if (questions[questionIndex].answers[i][0] === false) {
+                $(posArray[i]).empty();
                 var button = createButton("wrong");
                 button.text(questions[questionIndex].answers[i][1]);
-                buttonDiv.append(button);
+                $(posArray[i]).append(button);
             } else if (questions[questionIndex].answers[i][0] === true) {
+                $(posArray[i]).empty();
                 var button = createButton("right");
                 button.text(questions[questionIndex].answers[i][1]);
-                buttonDiv.append(button);
+                $(posArray[i]).append(button);
             }
         }
-
-        $("#test").html(buttonDiv);
+        questionCount++;
+        questionIndex++;
     }
 
-    createQuestionInterface(0);
+    $("#communication").on("click", function () {
+        createQuestionInterface();
 
-    $(".btn").on("click", function () {
 
-        console.log($(this).val());
+    });
 
+    $(document).on("click", ".btn", function () {
+        createQuestionInterface();
 
     });
 
 
 
-    console.log(questions[1].answers[0][0]);
 });
 
