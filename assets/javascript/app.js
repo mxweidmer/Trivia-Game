e18c2e17e5de2f740b80b76b06e4c36632f67a3a
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-01 18:18:22 -0800

    reworked questions object, wrote function to dynamically generate buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,60 +5,95 @@ $(document).ready(function () {
 
     var correct = 0;
     var incorrect = 0;
+
     var questions =
         [
             {
+                question: "What is the capitol of the US?",
+                answers: [
+                    [false, "New York City"],
+                    [true, "Washington DC"],
+                    [false, "Chicago"],
+                    [false, "San Francisco"]
+                ]
+            },
+            {
                 question: "a question",
-                answers: {
-                    answer: "1",
-                    answer: "2",
-                    trueAnswer: "3",
-                    answer: "4"
-                }
+                answers: [
+                    [false, "answer"],
+                    [true, "answer"],
+                    [false, "answer"],
+                    [false, "answer"]
+                ]
             },
             {
-                question: "a second question",
-                answers: {
-                    answer: "5",
-                    answer: "6",
-                    trueAnswer: "7",
-                    answer: "8"
-                }
+                question: "a question",
+                answers: [
+                    [false, "answer"],
+                    [true, "answer"],
+                    [false, "answer"],
+                    [false, "answer"]
+                ]
             },
             {
-                question: "a second question",
-                answers: {
-                    answer: "5",
-                    answer: "6",
-                    trueAnswer: "7",
-                    answer: "8"
-                }
+                question: "a question",
+                answers: [
+                    [false, "answer"],
+                    [true, "answer"],
+                    [false, "answer"],
+                    [false, "answer"]
+                ]
             },
             {
-                question: "a second question",
-                answers: {
-                    answer: "5",
-                    answer: "6",
-                    trueAnswer: "7",
-                    answer: "8"
-                }
-            },
-            {
-                question: "a second question",
-                answers: {
-                    answer: "5",
-                    answer: "6",
-                    trueAnswer: "7",
-                    answer: "8"
-                }
+                question: "a question",
+                answers: [
+                    [false, "answer"],
+                    [true, "answer"],
+                    [false, "answer"],
+                    [false, "answer"]
+                ]
             }
         ]
 
 
-    console.log(questions[1].answers.answer);
-    console.log(questions[1].answers.trueAnswer);
-    console.log(questions[1].question);
+    function createButton(answerType) {
+        var button = $("<button>");
+        button.attr("value", answerType);
+        button.attr("type", "button");
+        button.addClass("btn");
+        return button;
+    }
+
+    function createQuestionInterface(questionIndex) {
+
+        var buttonDiv = $("<div>");
+
+        for (var i = 0; i < questions[questionIndex].answers.length; i++) {
+            if (questions[questionIndex].answers[i][0] === false) {
+                var button = createButton("wrong");
+                button.text(questions[questionIndex].answers[i][1]);
+                buttonDiv.append(button);
+            } else if (questions[questionIndex].answers[i][0] === true) {
+                var button = createButton("right");
+                button.text(questions[questionIndex].answers[i][1]);
+                buttonDiv.append(button);
+            }
+        }
+
+        $("#test").html(buttonDiv);
+    }
+
+    createQuestionInterface(0);
+
+    $(".btn").on("click", function () {
+
+        console.log($(this).val());
 
 
+    });
+
+
+
+    console.log(questions[1].answers[0][0]);
 });
 
